5901c03efce8663f1c39ac259f359fd863fcaaca
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-04-26 13:50:10 +0300

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
+
+function handleChange(){
+    const Change = 'Entering password...'
+console.log(Change)
+}
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input onChange={handleChange} type="password"></input>
+        </div>
     )
 }
 
